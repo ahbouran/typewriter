c78bb70e1c18f8710f585f85fc3da9a6d94171e2
Ali <alihbouran@gmail.com>
2022-05-10 14:49:40 -0700

    refactored to include a new line

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,8 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 let delay = 0;
 for (let char of sentence) {
   setTimeout(() => 
-    process.stdout.write(char)
+  process.stdout.write(char)
     //console.log(char)
   , delay) 
   delay += 100
